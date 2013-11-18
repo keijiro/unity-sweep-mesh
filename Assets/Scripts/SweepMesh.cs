8396353757b68cd01e01ed14bed2345cdea009e8
Keijiro Takahashi <keijiro@MSX.local>
2013-11-19 08:50:07 +0900

    Profile generation.

--- Assets/Scripts/SweepMesh.cs
+++ Assets/Scripts/SweepMesh.cs
@@ -5,7 +5,8 @@ public class SweepMesh : MonoBehaviour
 {
     #region Public properties
 
-    public int division = 800;
+    public int division = 100;
+    public int points = 4;
     public float thickness = 1.0f;
     public float noiseLevel = 0.0f;
     public float noiseFreq = 0.1f;
@@ -20,25 +21,26 @@ public class SweepMesh : MonoBehaviour
 
     #endregion
 
-    #region Base shape generator
+    #region Profile generator
 
-    Vector3[] CreateSquareBase ()
+    Vector3[] CreateRegularPolygonProfile (int points)
     {
-        var array = new Vector3[4];
-        array [0] = new Vector3 (-1, -1, 0);
-        array [1] = new Vector3 (+1, -1, 0);
-        array [2] = new Vector3 (+1, +1, 0);
-        array [3] = new Vector3 (-1, +1, 0);
-        return array;
+        var profile = new Vector3[points];
+        for (int i = 0; i < points; i++)
+        {
+            var theta = Mathf.PI * 2 / points * i;
+            profile [i] = new Vector3 (Mathf.Cos (theta), Mathf.Sin (theta), 0);
+        }
+        return profile;
     }
 
     #endregion
 
-    #region Curve function
+    #region Path generator
 
-    AnimationCurve[] CreateCurveXYZ ()
+    AnimationCurve[] CreatePathFromChildren ()
     {
-        var curves = new AnimationCurve[] {
+        var path = new AnimationCurve[] {
             new AnimationCurve (),
             new AnimationCurve (),
             new AnimationCurve (),
@@ -54,16 +56,16 @@ public class SweepMesh : MonoBehaviour
         {
             var position = point.localPosition;
             var up = point.localRotation * Vector3.up;
-            curves [0].AddKey (time, position.x);
-            curves [1].AddKey (time, position.y);
-            curves [2].AddKey (time, position.z);
-            curves [3].AddKey (time, up.x);
-            curves [4].AddKey (time, up.y);
-            curves [5].AddKey (time, up.z);
+            path [0].AddKey (time, position.x);
+            path [1].AddKey (time, position.y);
+            path [2].AddKey (time, position.z);
+            path [3].AddKey (time, up.x);
+            path [4].AddKey (time, up.y);
+            path [5].AddKey (time, up.z);
             time += delta;
         }
 
-        return curves;
+        return path;
     }
 
     #endregion
@@ -79,10 +81,10 @@ public class SweepMesh : MonoBehaviour
 
     void Update ()
     {
-        var shape = CreateSquareBase ();
-        var curves = CreateCurveXYZ ();
+        var profile = CreateRegularPolygonProfile (points);
+        var path = CreatePathFromChildren ();
 
-        var vertices = new Vector3[shape.Length * (division * 2 + 2)];
+        var vertices = new Vector3[profile.Length * (division * 2 + 2)];
         var offset = 0;
 
         for (var i = 0; i < division; i++)
@@ -90,14 +92,14 @@ public class SweepMesh : MonoBehaviour
             var t1 = (float)(i + 0) / division;
             var t2 = (float)(i + 1) / division;
 
-            var p1 = new Vector3 (curves [0].Evaluate (t1), curves [1].Evaluate (t1), curves [2].Evaluate (t1));
-            var p2 = new Vector3 (curves [0].Evaluate (t2), curves [1].Evaluate (t2), curves [2].Evaluate (t2));
+            var p1 = new Vector3 (path [0].Evaluate (t1), path [1].Evaluate (t1), path [2].Evaluate (t1));
+            var p2 = new Vector3 (path [0].Evaluate (t2), path [1].Evaluate (t2), path [2].Evaluate (t2));
 
-            var ny = new Vector3 (curves [3].Evaluate (t1), curves [4].Evaluate (t1), curves [5].Evaluate (t1)).normalized;
+            var ny = new Vector3 (path [3].Evaluate (t1), path [4].Evaluate (t1), path [5].Evaluate (t1)).normalized;
             var nz = (p2 - p1).normalized;
             var nx = Vector3.Cross (ny, nz);
 
-            foreach (var v in shape)
+            foreach (var v in profile)
             {
                 var t = thickness + Perlin.Fbm (p1 * noiseFreq, noiseFractal) * noiseLevel;
                 var p = p1 + nx * (v.x * t) + ny * (v.y * t);
@@ -107,46 +109,46 @@ public class SweepMesh : MonoBehaviour
         }
 
         // Head cap.
-        for (var i = 0; i < shape.Length; i++)
+        for (var i = 0; i < profile.Length; i++)
         {
             vertices [offset++] = vertices [i * 2];
         }
 
         // Tail cap.
         {
-            var bi = offset - shape.Length * 3;
-            for (var i = 0; i < shape.Length; i++)
+            var bi = offset - profile.Length * 3;
+            for (var i = 0; i < profile.Length; i++)
             {
                 vertices [offset++] = vertices [bi + i * 2];
             }
         }
 
-        var indices = new int[(division - 1) * shape.Length * 6 + (shape.Length - 2) * 6];
+        var indices = new int[(division - 1) * profile.Length * 6 + (profile.Length - 2) * 6];
         offset = 0;
 
         for (var i = 0; i < division - 1; i++)
         {
-            for (var i2 = 0; i2 < shape.Length; i2++)
+            for (var i2 = 0; i2 < profile.Length; i2++)
             {
-                var bi1 = 2 * (shape.Length * i + i2) + 1;
-                var bi2 = 2 * (shape.Length * i + (i2 + 1) % shape.Length);
+                var bi1 = 2 * (profile.Length * i + i2) + 1;
+                var bi2 = 2 * (profile.Length * i + (i2 + 1) % profile.Length);
 
                 indices [offset++] = bi1;
                 indices [offset++] = bi2;
-                indices [offset++] = bi2 + shape.Length * 2;
+                indices [offset++] = bi2 + profile.Length * 2;
 
                 indices [offset++] = bi1;
-                indices [offset++] = bi2 + shape.Length * 2;
-                indices [offset++] = bi1 + shape.Length * 2;
+                indices [offset++] = bi2 + profile.Length * 2;
+                indices [offset++] = bi1 + profile.Length * 2;
             }
         }
 
         // Make caps with a trignale fan.
         {
-            var bi1 = vertices.Length - shape.Length * 2;
-            var bi2 = vertices.Length - shape.Length;
+            var bi1 = vertices.Length - profile.Length * 2;
+            var bi2 = vertices.Length - profile.Length;
 
-            for (var i = 1; i < shape.Length - 1; i++)
+            for (var i = 1; i < profile.Length - 1; i++)
             {
                 indices [offset++] = bi1;
                 indices [offset++] = bi1 + i + 1;
